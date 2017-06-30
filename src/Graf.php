1be38a28c3ab5a474ab6b351e7b005042bb6f3a2
Alex Gendelman <AlexGe@office.local>
2017-06-30 18:51:55 +0300

    Initial commit

---
+++ src/Graf.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlexGe
+ * Date: 6/29/2017
+ * Time: 13:42
+ */
+
+namespace responseGraf;
+
+
+class Graf
+{
+
+}
---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlexGe
+ * Date: 6/29/2017
+ * Time: 14:27
+ */
+
+require('Graf.php');
+
